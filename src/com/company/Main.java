79fa6d5e7b0bd73ef5e11bd2156685773af17d99
hwangevgeniya <hwang.evgeniya@gmail.com>
2021-04-27 16:46:25 +0600

    Ready + extra

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,39 +1,70 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
 
     public static String ageAndTemperature(int age, int temperature) {
-        
-        return age + temperature;
+
+
+
+
+        if (age >=20 && age <=40 && temperature >= -20 && temperature <= 30 ){
+           // System.out.println("Можно идти гулять");
+            return "Можно идти гулять";
+        }
+
+        if (age < 28 && temperature > 0 && temperature < 28){
+            //System.out.println("Можно идти гулять");
+            return "Можно идти гулять";
+        }
+
+        if (age > 45 && temperature >- 10 && temperature < 25){
+           // System.out.println("Можно идти гулять");
+            return "Можно идти гулять";
+        }
+
+
+        return "Оставайтесь дома";
+
     }
 
 
+    public static int generateRandomAge(){
 
+        //Random rnd = new Random();
+        //int age = rnd.nextInt(int 100);
+
+        int min = 1;
+        int max = 100;
+        int diff = max - min;
+        Random random = new Random();
+        int age2 = random.nextInt(diff + 1) + min;
+
+        return age2;
+
+    }
+
+
+    public static String doWeWalk() {
+
+        return "Можно ли идти гулять";
+
+    }
 
     public static void main(String[] args) {
 	// write your code here
 
+        System.out.println(ageAndTemperature(30,10));
 
-        System.out.println(ageAndTemperature(15,30));
+        System.out.println(doWeWalk());
 
-        if (age >=20 && age <=40 && temperature >= -20 && temperature <= 30 ){
-            System.out.println("Можно идти гулять");
-        }
-
-        if (age < 28 && temperature > 0 && temperature < 28){
-            System.out.println("Можно идти гулять");
-        }
-
-        if (age > 45 && temperature >- 10 && temperature < 25){
-            System.out.println("Можно идти гулять");
-        }
-        default:
-        System.out.println("Отавайтесь дома!");
-
+        System.out.println(generateRandomAge());
 
 
     }
+
 
 
 
